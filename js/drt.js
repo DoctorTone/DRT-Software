f87f6b767b381ad9f275eb42441774f3873731e3
DoctorTone <drtonesoftware@gmail.com>
2017-10-03 11:46:35 +0100

    Added full page scrolling

--- js/drt.js
+++ js/drt.js
@@ -7,5 +7,11 @@
 
 
 $(document).ready(() => {
-
+    $('#fullPage').fullpage({
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
+        sectionsColor: ['#353535', '#1BBC9B', '#7E8F7C', '#808080'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['About', 'Demos', 'Blog', 'Contact']
+    });
 });
