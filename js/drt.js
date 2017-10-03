cc4ac13b0fb724a6a95216b7dff2cac0efc179bc
DoctorTone <drtonesoftware@gmail.com>
2017-10-03 13:33:06 +0100

    New landing page format

--- js/drt.js
+++ js/drt.js
@@ -8,10 +8,11 @@
 
 $(document).ready(() => {
     $('#fullPage').fullpage({
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
-        sectionsColor: ['#353535', '#1BBC9B', '#7E8F7C', '#808080'],
+        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage'],
+        sectionsColor: ['#353535', '#1BBC9B', '#7E8F7C', '#808080', '#808080'],
         navigation: true,
         navigationPosition: 'right',
-        navigationTooltips: ['About', 'Demos', 'Blog', 'Contact']
+        navigationTooltips: ['Home', 'About', 'Demos', 'Blog', 'Contact'],
+        verticalCentered: false
     });
 });
