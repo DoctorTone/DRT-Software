b221044ae0ceb56dd1961a29aa3bcd7641dd2a3e
DoctorTone <drtonesoftware@gmail.com>
2017-10-03 22:42:30 +0100

    Landing page for small mobile devices

--- js/drt.js
+++ js/drt.js
@@ -8,7 +8,7 @@
 
 $(document).ready(() => {
     $('#fullPage').fullpage({
-        anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', '5thPage'],
+        anchors: ['home', 'about', 'demos', 'blog', 'contact'],
         sectionsColor: ['#353535', '#1BBC9B', '#7E8F7C', '#808080', '#808080'],
         navigation: true,
         navigationPosition: 'right',
