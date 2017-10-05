4ae6c16f065040db218467ad45b23595a5426882
DoctorTone <drtonesoftware@gmail.com>
2017-10-06 00:38:26 +0100

    Responsive landing page, gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
-let gulp = require('gulp');
+var gulp = require('gulp');
+var babel = require('gulp-babel');
 
-gulp.task('default', function() {
-    // place code for your default task here
+gulp.task('build', function() {
+    return gulp.src("./js/drt.js")
+        .pipe(babel())
+        .pipe(gulp.dest("dist/js/"))
 });
+
+gulp.task('copy', function() {
+    gulp.src("index.html")
+        .pipe(gulp.dest('dist'));
+    gulp.src("css/*.css")
+        .pipe(gulp.dest("dist/css/"))
+});
+
